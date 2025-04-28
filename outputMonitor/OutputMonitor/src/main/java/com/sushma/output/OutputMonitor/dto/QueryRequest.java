48c99a1a4054715aaac0ca4cfc994827b0c43944
sushma <76868368+sushmaDandeboyina@users.noreply.github.com>
2025-04-28 08:44:58 +0530

    queryrequest dto is added

---
+++ outputMonitor/OutputMonitor/src/main/java/com/sushma/output/OutputMonitor/dto/QueryRequest.java
@@ -0,0 +1,22 @@
+package com.sushma.output.OutputMonitor.dto;
+
+public class QueryRequest {
+    private String raw_query;
+    private String timestamp; // optional - you can use LocalDateTime if needed
+
+    public String getRaw_query() {
+        return raw_query;
+    }
+
+    public void setRaw_query(String raw_query) {
+        this.raw_query = raw_query;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
