ae3521684a0ed438234d63ff51109e2cad2c93a4
sushma <76868368+sushmaDandeboyina@users.noreply.github.com>
2025-05-01 00:32:55 +0530

    stream-master dto

---
+++ outputMonitor/OutputMonitor/src/main/java/com/sushma/output/OutputMonitor/dto/StreamInfo.java
@@ -0,0 +1,76 @@
+package com.sushma.output.OutputMonitor.dto;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "stream_master")
+public class StreamInfo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long streamId;
+
+    private String streamName;
+    private String windowType;
+    private int windowSize;
+    private int windowVelocity;
+    private String dataSourceType;
+    private String dataSourcePath;
+
+    // Add getters and setters
+    public Long getStreamId() {
+        return streamId;
+    }
+
+    public void setStreamId(Long streamId) {
+        this.streamId = streamId;
+    }
+
+    public String getStreamName() {
+        return streamName;
+    }
+
+    public void setStreamName(String streamName) {
+        this.streamName = streamName;
+    }
+
+    public String getWindowType() {
+        return windowType;
+    }
+
+    public void setWindowType(String windowType) {
+        this.windowType = windowType;
+    }
+
+    public int getWindowSize() {
+        return windowSize;
+    }
+
+    public void setWindowSize(int windowSize) {
+        this.windowSize = windowSize;
+    }
+
+    public int getWindowVelocity() {
+        return windowVelocity;
+    }
+
+    public void setWindowVelocity(int windowVelocity) {
+        this.windowVelocity = windowVelocity;
+    }
+
+    public String getDataSourceType() {
+        return dataSourceType;
+    }
+
+    public void setDataSourceType(String dataSourceType) {
+        this.dataSourceType = dataSourceType;
+    }
+
+    public String getDataSourcePath() {
+        return dataSourcePath;
+    }
+
+    public void setDataSourcePath(String dataSourcePath) {
+        this.dataSourcePath = dataSourcePath;
+    }
+}
