424c12e6c8722f4acad223ed7e46afce6e50cc59
sushma <76868368+sushmaDandeboyina@users.noreply.github.com>
2025-05-01 00:21:28 +0530

    available streams added

--- base_frontend/src/App.jsx
+++ base_frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
+import { Routes, Route, useNavigate} from "react-router-dom";
 import Step1DatabaseName from "./components/Step1DatabaseName";
 import Step2AddColumns from "./components/Step2AddColumns";
 import Step3Aggregation from "./components/Step3Aggregation";
 import Dashboard from "./components/Dashboard";
 import InputMonitor from "./components/InputMonitor";
-
+import StreamHome from "./components/StreamHome";
 
 const App = () => {
   const [streamName, setStreamname] = useState("");
@@ -15,15 +16,20 @@ const App = () => {
   const [dataSourcePath, setDataSourcePath] = useState("");
   const [streams, setStreams] = useState([]);
   const [columns, setColumns] = useState([]);
-  const [streamId, setStreamId] = useState(null);  // New state to store streamId
-  const [currentStep, setCurrentStep] = useState(5);
-  // Add this state:
-//const [isMonitorReady, setIsMonitorReady] = useState(false);
-
-
+  const [streamId, setStreamId] = useState(null); // New state to store streamId
+  //const [currentStep, setCurrentStep] = useState(0);
+  const navigate = useNavigate();
+ // const location = useLocation();
 
   const handleNextStep1 = async () => {
-    if (streamName && windowType && windowSize && windowVelocity && dataSourceType && dataSourcePath) {
+    if (
+      streamName &&
+      windowType &&
+      windowSize &&
+      windowVelocity &&
+      dataSourceType &&
+      dataSourcePath
+    ) {
       const payload = {
         streamName,
         windowType,
@@ -34,13 +40,16 @@ const App = () => {
       };
 
       try {
-        const response = await fetch("http://localhost:8081/ingestion/stream-master", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify(payload)
-        });
+        const response = await fetch(
+          "http://localhost:8081/ingestion/stream-master",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(payload),
+          }
+        );
         console.log("Payload to be sent:", payload);
         if (!response.ok) {
           throw new Error("Failed to submit stream info.");
@@ -51,7 +60,8 @@ const App = () => {
         if (result && result.streamId) {
           setStreamId(result.streamId); // Ensure streamId is set correctly
         }
-        setCurrentStep(2); // Transition to Step 2
+        // setCurrentStep(2); // Transition to Step 2
+        navigate("/step2");
       } catch (error) {
         console.error("Error submitting:", error);
         alert("Error submitting data to backend.");
@@ -63,83 +73,182 @@ const App = () => {
 
   const handleNextStep2 = (selectedColumns) => {
     setColumns(selectedColumns);
-    setCurrentStep(3); // Transition to Step 3 after Step 2
+    // setCurrentStep(3); // Transition to Step 3 after Step 2
+    navigate("/step3");
   };
 
   const handleNextStep3 = async () => {
     try {
       // Hit the create-table API on Step 3
-      const response1 = await fetch(`http://localhost:8081/api/tables/create-table/${streamId}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-      });
+      const response1 = await fetch(
+        `http://localhost:8081/api/tables/create-table/${streamId}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
 
       if (!response1.ok) {
         throw new Error("Failed to create table.");
       }
+
+      // If successful
       alert("Table created successfully!");
-      setCurrentStep(4); // You can optionally reset state or redirect after creation
+      // setCurrentStep(4); // You can optionally reset state or redirect after creation
+      navigate("/inputmonitor");
     } catch (error) {
       console.error("Error creating table:", error);
       alert("Error creating table.");
     }
     console.log("Moving to InputMonitor step");
-
   };
 
   console.log(streams);
   //console.log(isMonitorReady);
+
+  const handleBack = () => {
+    // Navigate to the previous page
+    navigate(-1);
+  };
+
+  //   return (
+  //     <div>
+  //     {currentStep === 0 && (
+  //   <StreamHome
+  //     setStreamId={setStreamId}
+  //     setStreamname={setStreamname}
+  //     setColumns={setColumns}
+  //     setCurrentStep={setCurrentStep}
+  //   />
+  // )}
+
+  //       {currentStep === 1 && (
+  //         <Step1DatabaseName
+  //           streamName={streamName}
+  //           setStreamname={setStreamname}
+  //           windowType={windowType}
+  //           setWindowtype={setWindowtype}
+  //           windowSize={windowSize}
+  //           setWindowsize={setWindowsize}
+  //           windowVelocity={windowVelocity}
+  //           setWindowvelocity={setWindowvelocity}
+  //           dataSourceType={dataSourceType}
+  //           setDataSourceType={setDataSourceType}
+  //           dataSourcePath={dataSourcePath}
+  //           setDataSourcePath={setDataSourcePath}
+  //           onNext={handleNextStep1}
+  //         />
+  //       )}
+  //       {currentStep === 2 && (
+  //         <Step2AddColumns
+  //           streamId={streamId}
+  //           onNext={handleNextStep2}
+  //         />
+  //       )}
+  //       {currentStep === 3 && (
+  //   <Step3Aggregation
+  //   columns={columns}
+  //   onNext={handleNextStep3} // Call the handleNextStep3 when "Next" is clicked
+  //   />
+  // )}
+  //      {currentStep === 4 && (
+  //   <InputMonitor
+  //     streamId={streamId}
+  //     onReady={() => {
+  //       //setIsMonitorReady(true);
+  //       setCurrentStep(5); // Go to dashboard once ready
+  //     }}
+  //   />
+  // )}
+
+  // {currentStep === 5 && (
+  //   <Dashboard streamId={streamId} streamName={streamName}     columns={columns}
+  // />
+  // )}
+
+  //     </div>
+  //   );
   return (
     <div>
-      {currentStep === 1 && (
-        <Step1DatabaseName
-          streamName={streamName}
-          setStreamname={setStreamname}
-          windowType={windowType}
-          setWindowtype={setWindowtype}
-          windowSize={windowSize}
-          setWindowsize={setWindowsize}
-          windowVelocity={windowVelocity}
-          setWindowvelocity={setWindowvelocity}
-          dataSourceType={dataSourceType}
-          setDataSourceType={setDataSourceType}
-          dataSourcePath={dataSourcePath}
-          setDataSourcePath={setDataSourcePath}
-          onNext={handleNextStep1}
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <StreamHome
+              setStreamId={setStreamId}
+              setStreamname={setStreamname}
+              setColumns={setColumns}
+              //setCurrentStep={() => navigate("/step1")}
+            />
+          }
         />
-      )}
-      {currentStep === 2 && (
-        <Step2AddColumns
-          streamId={streamId}
-          onNext={handleNextStep2}
+        <Route
+          path="/step1"
+          element={
+            <Step1DatabaseName
+              streamName={streamName}
+              setStreamname={setStreamname}
+              windowType={windowType}
+              setWindowtype={setWindowtype}
+              windowSize={windowSize}
+              setWindowsize={setWindowsize}
+              windowVelocity={windowVelocity}
+              setWindowvelocity={setWindowvelocity}
+              dataSourceType={dataSourceType}
+              setDataSourceType={setDataSourceType}
+              dataSourcePath={dataSourcePath}
+              setDataSourcePath={setDataSourcePath}
+              onNext={handleNextStep1}
+              onBack={handleBack}
+            />
+          }
         />
-      )}
-      {currentStep === 3 && (
-  <Step3Aggregation
-  columns={columns}
-  onNext={handleNextStep3} // Call the handleNextStep3 when "Next" is clicked
-  />
-)}
-     {currentStep === 4 && (
-  <InputMonitor
-    streamId={streamId}
-    onReady={() => {
-      //setIsMonitorReady(true);
-      setCurrentStep(5); // Go to dashboard once ready
-    }}
-  />
-)}
-
-{currentStep === 5 && (
-  <Dashboard streamId={streamId} streamName={streamName}     columns={columns}   
-/>
-)}
-
+        <Route
+          path="/step2"
+          element={
+            <Step2AddColumns
+              streamId={streamId}
+              onNext={handleNextStep2}
+              onBack={handleBack}
+            />
+          }
+        />
+        <Route
+          path="/step3"
+          element={
+            <Step3Aggregation
+              columns={columns}
+              onNext={handleNextStep3}
+              onBack={handleBack}
+            />
+          }
+        />
+        <Route
+          path="/input-monitor"
+          element={
+            <InputMonitor
+              streamId={streamId}
+              onReady={() => navigate("/dashboard")}
+              onBack={handleBack}
+            />
+          }
+        />
+        <Route
+          path="/dashboard"
+          element={
+            <Dashboard
+              streamId={streamId}
+              streamName={streamName}
+              columns={columns}
+              onBack={handleBack}
+            />
+          }
+        />
+      </Routes>
     </div>
   );
- 
 };
 
 export default App;
