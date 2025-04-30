ac7e3895c4ac89626ddba0ad296a24e93e7d9932
sushma <76868368+sushmaDandeboyina@users.noreply.github.com>
2025-05-01 00:20:58 +0530

    added streams in homepage and changed homepage

--- base_frontend/src/main.jsx
+++ base_frontend/src/main.jsx
@@ -6,12 +6,25 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Router>
+//       <Routes>
+//         <Route path="*" element={<App />} />
+//       </Routes>
+//     </Router>
+//   </React.StrictMode>
+//   document.getElementById("root")
+
+// );
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route path="*" element={<App />} />
-      </Routes>
+      <App />
     </Router>
   </React.StrictMode>
 );
