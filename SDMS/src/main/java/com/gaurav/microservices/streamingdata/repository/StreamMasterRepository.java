de409a2a26b4404e340286ccd533a4f9736d19d5
sushma <76868368+sushmaDandeboyina@users.noreply.github.com>
2025-05-01 18:37:16 +0530

    Delete SDMS directory

--- SDMS/src/main/java/com/gaurav/microservices/streamingdata/repository/StreamMasterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gaurav.microservices.streamingdata.repository;
-
-import com.gaurav.microservices.streamingdata.entity.StreamMasterEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StreamMasterRepository extends JpaRepository<StreamMasterEntity, Long> {
-}
--- SDMS/src/main/java/com/gaurav/microservices/streamingdata/repository/StreamQueryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gaurav.microservices.streamingdata.repository;
-
-import com.gaurav.microservices.streamingdata.entity.StreamQueryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StreamQueryRepository extends JpaRepository<StreamQueryEntity, Long> {
-}
